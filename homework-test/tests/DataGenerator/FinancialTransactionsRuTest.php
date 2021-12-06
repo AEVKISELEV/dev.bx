9f8c151cc8a9c5d61379c10d91055f50e92351f3
AEVKISELEV <aevkiselev@yandex.ru>
2021-12-06 04:37:14 +0200

    homework-testing

--- homework-test/tests/DataGenerator/FinancialTransactionsRuTest.php
+++ homework-test/tests/DataGenerator/FinancialTransactionsRuTest.php
@@ -1,20 +1,52 @@
 <?php
 
-class FinancialTransactionsRuTest extends \PHPUnit\Framework\TestCase
+namespace App\DataGenerator;
+
+use PHPUnit\Framework\TestCase;
+
+class FinancialTransactionsRuTest extends TestCase
 {
+
 	public function getValidateFailSamples(): array
 	{
 		return [
 			'empty' => [
 				[],
 			],
-			'filled but empty' => [
+			'Valid Mandatory field with FALSE' => [
+				[
+					'Name' => False,
+					'PersonalAcc' => '1',
+					'BankName' => '1',
+					'BIC' => '1',
+					'CorrespAcc' => '1',
+				]
+			],
+			'Valid Mandatory field with one empty line' => [
 				[
 					'Name' => '',
-					'PersonalAcc' => '',
-					'BankName' => '',
-					'BIC' => '',
-					'CorrespAcc' => '',
+					'PersonalAcc' => '1',
+					'BankName' => '1',
+					'BIC' => '1',
+					'CorrespAcc' => '1',
+				]
+			],
+			'Valid Type' => [
+				[
+					'Name' => [1],
+					'PersonalAcc' => 3,7,
+					'BankName' => 5,
+					'BIC' => '1',
+					'CorrespAcc' => '1',
+				]
+			],
+			'Valid Maximum Length(9 symbol in BIC)' => [
+				[
+					'Name' => '1',
+					'PersonalAcc' => '1',
+					'BankName' => '1',
+					'BIC' => '01234567890',
+					'CorrespAcc' => '1',
 				]
 			],
 		];
@@ -25,6 +57,7 @@ class FinancialTransactionsRuTest extends \PHPUnit\Framework\TestCase
 	 *
 	 * @param array $fields
 	 */
+
 	public function testValidateFail(array $fields): void
 	{
 		$dataGenerator = new \App\DataGenerator\FinancialTransactionsRu();
@@ -36,6 +69,9 @@ class FinancialTransactionsRuTest extends \PHPUnit\Framework\TestCase
 		static::assertFalse($result->isSuccess());
 	}
 
+
+
+
 	public function testThatValidateSuccess(): void
 	{
 		$dataGenerator = new \App\DataGenerator\FinancialTransactionsRu();
@@ -44,8 +80,10 @@ class FinancialTransactionsRuTest extends \PHPUnit\Framework\TestCase
 
 		$dataGenerator
 			->setName('Name')
+			->setField('BIC', false)
 			->setBIC('BIC')
 			->setBankName('BankName')
+			->addFields(['Name'=> 'Name'])
 			->setCorrespondentAccount('CorrespondentAccount')
 			->setPersonalAccount('CorrespondentAccount')
 		;
@@ -54,15 +92,65 @@ class FinancialTransactionsRuTest extends \PHPUnit\Framework\TestCase
 
 		static::assertTrue($result->isSuccess());
 	}
+	public function getValidateRightSamples(): array
+	{
+		return [
+			'Valid Mandatory field with FALSE' => [
+				[
+					'Name' => False,
+					'PersonalAcc' => '1',
+					'BankName' => '1',
+					'BIC' => '1',
+					'CorrespAcc' => '1',
+				]
+			],
+			'Valid Mandatory field with one empty line' => [
+				[
+					'Name' => '',
+					'PersonalAcc' => '1',
+					'BankName' => '1',
+					'BIC' => '1',
+					'CorrespAcc' => '1',
+				]
+			],
+			'Valid Type array' => [
+				[
+					'Name' => [1],
+					'PersonalAcc' => 3,
+					'BankName' => 5,
+					'BIC' => '1',
+					'CorrespAcc' => '1',
+				]
+			],
+			'Valid Type object and don`t validate length Name' => [
+				[
+					'Name' => $this->$fields,
+					'PersonalAcc' => '1',
+					'BankName' => '1',
+					'BIC' => '01234567890',
+					'CorrespAcc' => '1',
+				]
+			],
+		];
+	}
 
-	public function testGetData(): void
+	/**
+	 * @dataProvider getValidateRightSamples
+	 *
+	 * @param array $fields
+	 */
+	public function testGetDataRight($fields): void
 	{
 		$dataGenerator = new \App\DataGenerator\FinancialTransactionsRu();
 
-		$dataGenerator->setFields([]);
+		$dataGenerator->setFields($fields);
+		echo
 
 		$data = $dataGenerator->getData();
 
-		static::assertEquals('ST00012|Name=|PersonalAcc=|BankName=|BIC=|CorrespAcc=', $data);
+		$string = 'ST00012|Name=|PersonalAcc='.$fields["PersonalAcc"].'|BankName='.$fields["BankName"].'|BIC='.$fields["BIC"].'|CorrespAcc='.$fields["CorrespAcc"];
+
+		static::assertEquals($string, $data);
 	}
 }
+
